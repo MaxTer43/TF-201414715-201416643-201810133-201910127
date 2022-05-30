3b105279eeb4d65be71a587fac2486ead613b9ed
PerKimba <u201416643@upc.edu.pe>
2022-05-30 14:34:34 -0500

    datetime

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import dijkstra
+import datetime
 
 if __name__=='__main__':
+    time = datetime.datetime.now()
+    hour = time.strftime("%H")
+
     print("Waze en progreso")
     #g = dijkstra.Graph(9)
     #g.graph = [[2, 2, 1, 8, 6, 2, 1, 0, 9],
