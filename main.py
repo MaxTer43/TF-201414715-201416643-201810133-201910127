93729e84484c2f68a9810feb7cce7734cef27b0d
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2022-05-30 21:05:58 -0500

    close #1

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if __name__=='__main__':
     hour = time.strftime("%H")
 
     print("Waze en progreso")
-    #g = dijkstra.Graph(9)
-    #g.graph = [[2, 2, 1, 8, 6, 2, 1, 0, 9],
+    
+    # g = dijkstra.Graph(9)
+    # g.graph = [[2, 2, 1, 8, 6, 2, 1, 0, 9],
     #            [2, 2, 1, 8, 6, 2, 1, 0, 9],
     #            [2, 2, 1, 8, 6, 2, 1, 2, 9],
     #            [2, 2, 1, 8, 6, 2, 1, 0, 9],
@@ -18,4 +19,4 @@ if __name__=='__main__':
     #            [2, 2, 1, 8, 6, 2, 1, 0, 9]
     #            ]
  
-    #g.dijkstra(0)
+    # g.dijkstra(0)
